a4a45cc625c912ddc7146918511111a100ffb0fd
Emeka Obianom <3mekslondon@gmail.com>
2022-10-31 10:02:17 +0100

    result - ui design

--- src/Pages/Result.js
+++ src/Pages/Result.js
@@ -1,23 +1,18 @@
-
 import parse from "html-react-parser";
 import React, { useState } from "react";
 import { InterviewIcon, UserIcon } from "../helper";
 
-
-const Result = () => {
-
-     //chats
+const Result = ({ onChangePage }) => {
+  //chats
   let chatStore = [
     `${InterviewIcon} Your Result for this Quiz is. <br/>`,
-    `${InterviewIcon} My name is Mike. <br/>`,
+    `<h1>3/10<h1/>`,
   ];
 
-  //------- handlers -----  
-  function onEnterKeyHandle(event) {
-    if(event.key === 'Enter'){
-      alert(event.target.value);
-    } 
-  } 
+  //------- handlers -----
+  function onRestartHandle(event) {
+    onChangePage("welcome", {});
+  }
 
   //-------- engine ------
   const [chat, setChat] = useState(chatStore[0]);
@@ -30,17 +25,22 @@ const Result = () => {
   }, 1000);
 
   //stop using store
-  let endOfStoreChat = (chatCount == chatStore.length);
+  let endOfStoreChat = chatCount == chatStore.length;
   if (endOfStoreChat) {
     clearTimeout(timer);
   }
 
-    return (
-        <div>
-        <h3>Result</h3>
-        {parse(chat)}
-      </div>
-    );
-}
+  return (
+    <div>
+      <h3>Result</h3>
+      {parse(chat)}
+      {endOfStoreChat && (
+        <>
+          {UserIcon} <button onClick={onRestartHandle}>Restart</button>{" "}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Result;
